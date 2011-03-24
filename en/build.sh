8f53487250f674d7c32c20acf3c6cf2ba8da94f6
goran <goran.juric@nacional.hr>
2011-03-24 23:50:35 +0100

    Changed syntax of some programlistings

--- en/build.sh
+++ en/build.sh
@@ -5,9 +5,9 @@ export FOP=/Users/gog/Development/xml2pdf/fop-0.95
 export OUT=/Users/gog/Development/mail-server-book/en
 
 # Single page
-xsltproc --stringparam section.autolabel 1 --stringparam section.label.includes.component.label 1 --stringparam  html.stylesheet 'docbook.css' --xinclude -o output/html/index.html  $XSL/xhtml/onechunk.xsl book.xml
+xsltproc --stringparam section.autolabel 1 --stringparam section.label.includes.component.label 1 --stringparam  html.stylesheet 'docbook.css' --xinclude -o output/html/index.html  $XSL/gog-custom.xsl book.xml
 # Chunked
-xsltproc --stringparam section.autolabel 1 --stringparam section.label.includes.component.label 1 --stringparam  html.stylesheet 'docbook.css' --stringparam use.id.as.filename 1 --xinclude -o output/chunks/index.html  $XSL/xhtml/chunk.xsl book.xml
+xsltproc --stringparam section.autolabel 1 --stringparam section.label.includes.component.label 1 --stringparam  html.stylesheet 'docbook.css' --stringparam use.id.as.filename 1 --xinclude -o output/chunks/index.html  $XSL/gog-custom-chunked.xsl book.xml
 # PDF
-cd $FOP
-$FOP/fop -c $FOP/conf/fop.xconf -xml $OUT/book.xml -xsl $XSL/fo/docbook.xsl -param body.font.family Times-Roman -param title.font.family Times-Roman -pdf $OUT/output/book.pdf
+#cd $FOP
+#$FOP/fop -c $FOP/conf/fop.xconf -xml $OUT/book.xml -xsl $XSL/fo/docbook.xsl -param body.font.family Times-Roman -param title.font.family Times-Roman -pdf $OUT/output/book.pdf
